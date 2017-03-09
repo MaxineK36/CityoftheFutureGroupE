c3efac6159822ca3f7a652ad94643d5252a69066
MaxineK36 <maxine@algernon.com>
2017-03-09 08:25:16 -0500

    Update BrightnessAdjust.ino

--- BrightnessAdjust.ino
+++ BrightnessAdjust.ino
@@ -1,1 +1,29 @@
-/**/int led = 4;int brightness = 0;int buttonPin = 2;int posneg = 1; void setup() {  pinMode(led, OUTPUT);  pinMode(buttonPin, INPUT);  Serial.begin(9600);}void loop() {  delay(100);  brightness = brightness + (posneg*5);  digitalWrite(led, brightness);  Serial.println(brightness);  if (brightness>=255){    posneg = -1;  }  else if (brightness<=0){    posneg = 1;  }    }
+/*
+This is one of the first times we used the arduino, when we attempted to slowly adjust the brightness of an LED using code
+*/
+int led = 4;
+int brightness = 0;
+int buttonPin = 2;
+int posneg = 1;
+ 
+
+void setup() {
+  pinMode(led, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  delay(100);
+  brightness = brightness + (posneg*5);
+  digitalWrite(led, brightness);
+  Serial.println(brightness);
+  if (brightness>=255){
+    posneg = -1;
+  }
+  else if (brightness<=0){
+    posneg = 1;
+  }
+  
+  
+}
